caa08290889cf3a6b3a9b0eed9293a5abee5b2c7
M.Aung <m.aung@damstratechnology.com>
2024-09-05 12:14:13 +1200

    Codes from Frontend React side

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import router from "./router.jsx";
+import {RouterProvider} from "react-router-dom";
+import {ContextProvider} from "./contexts/ContextProvider.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ContextProvider>
+      <RouterProvider router={router} />
+    </ContextProvider>
   </StrictMode>,
 )
---
+++ frontend/src/views/Dashboard.jsx
@@ -0,0 +1,10 @@
+import PageComponent from "../components/PageComponent.jsx";
+
+export default function Dashboard() {
+  return (
+    <PageComponent
+      title='Dashboard'>
+      Dashboard Content
+    </PageComponent>
+  )
+}
