fa38e72f912468e65737930248353a736951aa47
minooaung <aungminoo83@gmail.com>
2024-10-09 13:15:56 +1300

    logo commented

--- frontend/src/components/GuestLayout.jsx
+++ frontend/src/components/GuestLayout.jsx
@@ -1,25 +1,25 @@
-import {Navigate, Outlet} from "react-router-dom";
-import {useStateContext} from "../contexts/ContextProvider.jsx";
+import { Navigate, Outlet } from "react-router-dom";
+import { useStateContext } from "../contexts/ContextProvider.jsx";
 
 export default function GuestLayout() {
-  const { userToken} = useStateContext();
+  const { userToken } = useStateContext();
 
   if (userToken) {
-    return <Navigate to='/' />
+    return <Navigate to="/" />;
   }
 
   return (
     <div>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
+          {/* <img
             alt="Your Company"
             src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
             className="mx-auto h-10 w-auto"
-          />
-          <Outlet/>
+          /> */}
+          <Outlet />
         </div>
       </div>
     </div>
-  )
+  );
 }
